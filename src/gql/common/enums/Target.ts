76dc10a25ca041b5247f3697889fc3a158767c14
Migushthe2nd <ihaverkamp@live.nl>
2021-08-28 15:19:18 +0200

    Update Target names

--- src/gql/common/enums/Target.ts
+++ src/gql/common/enums/Target.ts
@@ -1,13 +1,13 @@
 import {registerEnumType} from "@nestjs/graphql";
 
 export enum Target {
-    RESIDENTMENU = "ResidentMenu",
-    ENTRANCE = "Entrance",
-    FLAUNCH = "Flaunch",
-    SET = "Set",
-    PSL = "Psl",
-    MYPAGE = "MyPage",
-    NOTIFICATION = "Notification",
+    ResidentMenu = "ResidentMenu",
+    Entrance = "Entrance",
+    Flaunch = "Flaunch",
+    Set = "Set",
+    Psl = "Psl",
+    MyPage = "MyPage",
+    Notification = "Notification",
 }
 
 registerEnumType(Target, {
