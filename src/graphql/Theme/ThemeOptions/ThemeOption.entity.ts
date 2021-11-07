7343230834907e7b8a248fb8a73b06996775e468
Migushthe2nd <ihaverkamp@live.nl>
2021-11-07 23:50:56 +0100

    Part of 8c50a2871bd8687ceefa5fdc9a3c258aca5e6f4f

--- src/graphql/Theme/ThemeOptions/ThemeOption.entity.ts
+++ src/graphql/Theme/ThemeOptions/ThemeOption.entity.ts
@@ -1,4 +1,4 @@
-import {Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn} from "typeorm";
+import {Column, Entity, JoinColumn, ManyToOne, PrimaryColumn} from "typeorm";
 import {LayoutOptionValueEntity} from "../../LayoutOption/OptionValue/LayoutOptionValue.entity";
 import {ThemeEntity} from "../Theme.entity";
 
