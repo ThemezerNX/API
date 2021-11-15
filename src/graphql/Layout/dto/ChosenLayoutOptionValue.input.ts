451d3de5f90279389f8051ef950926b99d82e064
Migushthe2nd <ihaverkamp@live.nl>
2021-11-15 18:00:37 +0100

    Update ChosenLayoutOptionValue values description

--- src/graphql/Layout/dto/ChosenLayoutOptionValue.input.ts
+++ src/graphql/Layout/dto/ChosenLayoutOptionValue.input.ts
@@ -4,26 +4,28 @@ import {IsDecimal, IsHexColor, IsInt, IsNotEmpty, IsOptional, IsUUID, Length} fr
 @InputType()
 export class ChosenLayoutOptionValue {
 
+    private static ONLY_APPLICCABLE = "Only the applicable value field is required.";
+
     @Field({nullable: true})
     @IsUUID()
     uuid: string;
 
-    @Field({nullable: true, description: "Only one of the value fields is required."})
+    @Field({nullable: true, description: ChosenLayoutOptionValue.ONLY_APPLICCABLE})
     @IsInt()
     @IsOptional()
     integerValue?: number;
 
-    @Field({nullable: true, description: "Only one of the value fields is required."})
+    @Field({nullable: true, description: ChosenLayoutOptionValue.ONLY_APPLICCABLE})
     @IsDecimal()
     @IsOptional()
     decimalValue?: number;
 
-    @Field({nullable: true, description: "Only one of the value fields is required."})
+    @Field({nullable: true, description: ChosenLayoutOptionValue.ONLY_APPLICCABLE})
     @IsNotEmpty()
     @IsOptional()
     stringValue?: string;
 
-    @Field({nullable: true, description: "Only one of the value fields is required."})
+    @Field({nullable: true, description: ChosenLayoutOptionValue.ONLY_APPLICCABLE})
     @IsHexColor()
     @Length(8, 8) // TODO: check if this works
     @IsOptional()
