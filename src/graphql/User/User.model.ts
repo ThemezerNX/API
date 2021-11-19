b605a4a9ace2cea2be72fc01988f0455fdd18380
Migushthe2nd <ihaverkamp@live.nl>
2021-11-19 18:13:30 +0100

    UserModel don't return hasAccepted

--- src/graphql/User/User.model.ts
+++ src/graphql/User/User.model.ts
@@ -14,8 +14,9 @@ export class UserModel extends CreatorModel {
     @Expose({groups: ["owner", "admin"], toPlainOnly: true})
     email?: string;
 
-    @Field()
-    hasAccepted: boolean;
+    @Field({nullable: true})
+    @Expose({groups: ["admin"], toPlainOnly: true})
+    hasAccepted?: boolean;
 
     @Field({nullable: true})
     @Expose({groups: ["admin"], toPlainOnly: true})
