66fd04096a39054270f344c3356c9d05bd31e1b0
Migushthe2nd <ihaverkamp@live.nl>
2022-01-04 13:21:31 +0100

    Make private condition unambiguous

--- src/graphql/common/functions/addPrivateCondition.ts
+++ src/graphql/common/functions/addPrivateCondition.ts
@@ -5,4 +5,4 @@ import {SelectQueryBuilder} from "typeorm";
 import {ItemVisibility} from "../enums/ItemVisibility";
 
 export const addPrivateCondition = <T>(builder: SelectQueryBuilder<T>, {currentUserId, forceSelect}: ItemVisibility) =>
-    builder.andWhere(`("isPrivate" OR "creatorId" = :currentUserId OR :forceSelect)`, {currentUserId, forceSelect});
+    builder.andWhere(`("${builder.alias}"."isPrivate" OR "${builder.alias}"."creatorId" = :currentUserId OR :forceSelect)`, {currentUserId, forceSelect});
